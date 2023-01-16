03adf6f88b3e7e7820c0fd18595ac74ebed48124
Mohammed Gheith <mog@ellab.com>
2023-01-16 15:06:11 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace test_server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                IPAddress ip = IPAddress.Any;
+                int port = 8000;
+
+                TcpListener server = new TcpListener(ip, port);
+                server.Start();
+
+                Console.WriteLine("Server started...");
+
+                TcpClient client = server.AcceptTcpClient();
+
+                Console.WriteLine("Client connected...");
+
+                NetworkStream stream = client.GetStream();
+
+                while (true)
+                {
+                    byte[] message = new byte[1024];
+                    int bytesRead = stream.Read(message, 0, message.Length);
+
+                    if (bytesRead == 0)
+                        break;
+
+                    Console.WriteLine("Received: " + Encoding.ASCII.GetString(message, 0, bytesRead));
+
+                    byte[] data = Encoding.ASCII.GetBytes("Hello from server");
+                    stream.Write(data, 0, data.Length);
+                    Console.WriteLine("Sent: Hello from server");
+                }
+
+                client.Close();
+                server.Stop();
+            }
+        }
+    }
+}
